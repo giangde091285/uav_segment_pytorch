d38fa0982289700063b712fd8495960432ccef9d
RefineM <112970219+RefineM@users.noreply.github.com>
2023-09-22 21:39:19 +0800

    23-9-22 change loss and acc

---
+++ utils/loss.py
@@ -0,0 +1,78 @@
+import numpy as np
+import torch
+from torch import *
+from torch import nn
+import torch.functional as F
+
+"""
+
+    loss function
+
+    * dice loss 
+    * focal loss 
+
+"""
+
+class DiceLoss(nn.Module):
+    def __init__(self, class_num:int) -> None:
+        super(DiceLoss, self).__init__()
+        self.class_num = class_num 
+
+    def forward(self, input:torch.Tensor, target:torch.Tensor):
+        # input:(N C H W) -> (N H W C) -> (NHW=n C) 
+        input = input.permute(0, 2, 3, 1).flatten(0,2)
+        input = F.softmax(input, 1)  # softmax
+        # target:(N H W) -> (N H W C) -> (NHW=n C)
+        target = F.one_hot(target, self.class_num).flatten(0,2)
+
+        # intersect = （input * target).sum()
+        intersect = (input * target).sum(dim=0)  #(c)
+        a = input.sum(dim=0) #(c)
+        b = target.sum(dim=0) #(c)
+
+        dice_loss = 1 - 2 * intersect/(a + b) #(c)
+        return dice_loss.mean() 
+    
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=[1, 1, 1], gamma=2, class_num=3) -> None:
+        super(FocalLoss, self).__init__()
+        self.alpha = torch.Tensor(alpha).unsqueeze(dim=1)
+        self.gamma = gamma
+        self.class_num = class_num
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor, avg: bool = True):
+        # input: (N C H W) -> (N H W C) -> (N*H*W=n C)
+        input = input.permute(0, 2, 3, 1)
+        input = input.flatten(0, 2)
+        input = F.softmax(input, 1)  # softmax
+
+        # target: (N H W) -> (N*H*W) -> (N*H*W=n C)
+        target = target.flatten(0, 2)
+        target = F.one_hot(target, self.class_num)
+
+        # 把input中，target=0处对应位置的值换为1, 便于计算log(p)
+        P_1 = torch.masked_fill(input, ~target.bool(), 1)  # (n C)
+        # 把input中，target=0处对应位置的值换为0, 便于计算p
+        P_0 = torch.masked_fill(input, ~target.bool(), 0)  # (n C)
+
+        # (1) - alpha * log(p)
+        logP = torch.log(P_1)  # (n C)
+        weight_logP = - torch.mm(logP, self.alpha).squeeze()  # (n)
+
+        # (2)  (1-p)**gamma
+        # 沿列方向求和，求出每一个像元属于所有类的loss之和
+        p = torch.sum(P_0, dim=1)  # (n)
+        fac = torch.pow((1 - p), self.gamma)  # (n)
+
+        loss = torch.mul(weight_logP, fac)  # (n)
+
+        if avg:
+            loss = loss.mean()
+        return loss
+ 
+
+        
+
+
+
---
+++ utils/metric.py
@@ -0,0 +1,88 @@
+import numpy as np
+import torch
+from torch import *
+from torch import nn
+
+"""
+    metric 
+   
+    * IOU
+    * Dice Score
+    * Acc
+    * Confusion Mat
+
+"""
+
+class Metric(nn.Module):
+    def __init__(self):
+        super(Metric, self).__init__()
+
+    def cal_intersect_and_union(self, input: torch.Tensor, target: torch.Tensor, class_num: int):
+        # input (N C H W)-> (N H W C)-> (NHW C)-> (NHW)
+        input = input.permute(0, 2, 3, 1).flatten(0, 2)
+        input = torch.argmax(input, dim=1)
+        input = torch.as_tensor(input, dtype=torch.float32)
+        # target (N H W) -> (NHW)
+        target = target.flatten(0, 2)
+
+        # A ∩ B = TP
+        intersect = input[input == target]
+        intersect = torch.histc(intersect, class_num, min=0, max=class_num-1).cpu()  # torch.histc()只接受float类型的tensor
+        # A = TP + FP
+        tp_plus_fp = torch.histc(input, class_num, min=0, max=class_num - 1).cpu()
+        # B = TP + TN
+        tp_plus_tn = torch.histc(target, class_num, min=0, max=class_num - 1).cpu()
+        # A ∪ B
+        union = tp_plus_fp + tp_plus_tn - intersect
+      
+        return intersect, union, tp_plus_fp, tp_plus_tn
+
+    def IOU(self, input: torch.Tensor, target: torch.Tensor, class_num: int):
+        # iou = (A ∩ B)/(A ∪ B) = TP/(TP+TN+FP)
+        intersect, union, _, _= self.cal_intersect_and_union(input, target, class_num)
+        iou = intersect/union
+        return iou # (c)
+    
+    def DiceScore(self, input: torch.Tensor, target: torch.Tensor, class_num: int):
+        # Dice = 2*(A ∩ B)/(A + B) = 2*TP/(2*TP+TN+FP)
+        intersect, _, tp_plus_fp, tp_plus_fn = self.cal_intersect_and_union(input, target, class_num)
+        dice = 2 * intersect/(tp_plus_fp + tp_plus_fn)
+        return dice # (c)
+    
+    def Acc(self, input: torch.Tensor, target: torch.Tensor, class_num: int):
+        # Acc = TP / total_num
+        intersect, _, _, _= self.cal_intersect_and_union(input, target, class_num)
+        total_num = input.size(0) * input.size(2) * input.size(3)
+        acc = intersect/total_num
+        return acc # (c)
+
+
+class ConfusionMat(nn.Module):
+    def __init__(self) -> None:
+        super(ConfusionMat, self).__init__()
+
+    def cfm(self, input, target, class_num):
+        # target (N H W) -> (NHW)
+        target = target.flatten(0,2)
+        # input (N C H W) -> (N H W C) -> (NHW C) -> (NHW)
+        input = input.permute(0, 2, 3, 1).flatten(0,2)
+        input = torch.argmax(input, dim=1)
+
+        # get category
+        categroy = class_num * target + input
+        categroy = np.asarray(categroy.cpu(), dtype=np.uint8) 
+        # get confusion mat  -- row : gt , col : pre
+        confusion_mat = np.zeros((class_num*class_num), dtype=np.uint8)
+        for i in np.linspace(0, confusion_mat.size, endpoint=False, num=confusion_mat.size, dtype=np.int64):
+            confusion_mat[i] = np.sum(categroy == i)
+        confusion_mat = confusion_mat.reshape(class_num, class_num)
+        return confusion_mat
+
+
+
+
+
+
+
+
+
