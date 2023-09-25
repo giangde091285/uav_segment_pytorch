bb1fcc0e942685b0b23559af8e50f6266e2f5d43
RefineM <112970219+RefineM@users.noreply.github.com>
2023-09-25 13:20:57 +0800

    Update labelRGB.py

--- utils/labelRGB.py
+++ utils/labelRGB.py
@@ -24,39 +24,10 @@ else:
     raise Exception('no such dataset')
 
 
-# def label2RGB(mask_path, vis_path):
-
-#     new_mask = np.array(Image.open(mask_path)).astype(np.uint8)
-
-#     cm = np.array(list(map.values())).astype(np.uint8)
-
-#     color_img = cm[new_mask]
-
-#     color_img = Image.fromarray(np.uint8(color_img))
-
-#     color_img.save(vis_path)
-
-
 def labeltoRGB(input:np.array, vis_path):
-
-
     cm = np.array(list(map.values())).astype(np.uint8)
-
     color_img = cm[input]
-
     color_img = Image.fromarray(np.uint8(color_img))
-
     color_img.save(vis_path)
 
 
-# if __name__ == '__main__':
-
-#     mask_root = r'E:\studio\研究生期间\研0\1.Data\whu_res\output-23-9-22'
-#     mask_list = os.listdir(mask_root)
-#     vis_root = r'E:\studio\研究生期间\研0\1.Data\whu_res\vis\pre'
-
-#     for i in range(len(mask_list)):
-#         mask_path = os.path.join(mask_root, mask_list[i])
-#         mask_name = 'vis' + mask_list[i]
-#         vis_path = os.path.join(vis_root, mask_name)
-#         label2RGB(mask_path, vis_path)
